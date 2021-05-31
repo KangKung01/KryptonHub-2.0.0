4e3866650265e29458c449d0b4cf0e4538b57522
Kang Kung <67919863+KangKung01@users.noreply.github.com>
2021-05-31 15:40:21 +0700

    Update ListGame.lua

--- ListGame.lua
+++ ListGame.lua
@@ -1,5 +1,25 @@
-if game.PlaceId == 1 then
-  
-  elseif game.PlaceId == 2 then
-  
+if game.PlaceId == 394506555 then
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/KangKung01/KryptonHub-2.0.0/main/AllScript/394506555.lua"))();
+ elseif game.PlaceId == 0 then
+  loadstring(game:HttpGet(""))();
+  elseif game.PlaceId == 0 then
+  loadstring(game:HttpGet(""))();
+  elseif game.PlaceId == 0 then
+  loadstring(game:HttpGet(""))();
+  elseif game.PlaceId == 0 then
+  loadstring(game:HttpGet(""))();
+  elseif game.PlaceId == 0 then
+  loadstring(game:HttpGet(""))();
+  elseif game.PlaceId == 0 then
+  loadstring(game:HttpGet(""))();
+  elseif game.PlaceId == 0 then
+  loadstring(game:HttpGet(""))();
+  elseif game.PlaceId == 0 then
+  loadstring(game:HttpGet(""))();
+  elseif game.PlaceId == 0 then
+  loadstring(game:HttpGet(""))();
+  elseif game.PlaceId == 0 then
+  loadstring(game:HttpGet(""))();
+  elseif game.PlaceId == 0 then
+  loadstring(game:HttpGet(""))();
   end
