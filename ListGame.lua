48e812b396080b2cfedf4f48f4f250d7d6d96baa
Kang Kung <67919863+KangKung01@users.noreply.github.com>
2021-05-31 16:21:07 +0700

    Update ListGame.lua

--- ListGame.lua
+++ ListGame.lua
@@ -1,25 +1,23 @@
-if game.PlaceId == 394506555 then
+if game.PlaceId == 1499872953 then
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/KangKung01/KryptonHub-2.0.0/main/AllScript/1499872953.lua"))();
+ elseif game.PlaceId == 2116452049 then
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/KangKung01/KryptonHub-2.0.0/main/AllScript/2116452049.lua"))();
+  elseif game.PlaceId == 2281639237 then
+  loadstring(game:HttpGet("https://github.com/KangKung01/KryptonHub-2.0.0/blob/main/AllScript/2281639237.lua"))();
+  elseif game.PlaceId == 394506555 then
   loadstring(game:HttpGet("https://raw.githubusercontent.com/KangKung01/KryptonHub-2.0.0/main/AllScript/394506555.lua"))();
- elseif game.PlaceId == 0 then
-  loadstring(game:HttpGet(""))();
-  elseif game.PlaceId == 0 then
-  loadstring(game:HttpGet(""))();
-  elseif game.PlaceId == 0 then
-  loadstring(game:HttpGet(""))();
-  elseif game.PlaceId == 0 then
-  loadstring(game:HttpGet(""))();
-  elseif game.PlaceId == 0 then
-  loadstring(game:HttpGet(""))();
-  elseif game.PlaceId == 0 then
-  loadstring(game:HttpGet(""))();
-  elseif game.PlaceId == 0 then
-  loadstring(game:HttpGet(""))();
-  elseif game.PlaceId == 0 then
-  loadstring(game:HttpGet(""))();
-  elseif game.PlaceId == 0 then
-  loadstring(game:HttpGet(""))();
-  elseif game.PlaceId == 0 then
-  loadstring(game:HttpGet(""))();
-  elseif game.PlaceId == 0 then
-  loadstring(game:HttpGet(""))();
+  elseif game.PlaceId == 4520749081 then
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/KangKung01/KryptonHub-2.0.0/main/AllScript/4520749081.lua"))();
+  elseif game.PlaceId == 5170554573 then
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/KangKung01/KryptonHub-2.0.0/main/AllScript/5170554573.lua"))();
+  elseif game.PlaceId == 5303541547 then
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/KangKung01/KryptonHub-2.0.0/main/AllScript/5303541547.lua"))();
+  elseif game.PlaceId == 5835263912 then
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/KangKung01/KryptonHub-2.0.0/main/AllScript/5835263912.lua"))();
+  elseif game.PlaceId == 6186867282 then
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/KangKung01/KryptonHub-2.0.0/main/AllScript/6186867282.lua"))();
+  elseif game.PlaceId == 6329844902 then
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/KangKung01/KryptonHub-2.0.0/main/AllScript/6329844902.lua"))();
+  elseif game.PlaceId == 6381829480 then
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/KangKung01/KryptonHub-2.0.0/main/AllScript/6381829480.lua"))();
   end
